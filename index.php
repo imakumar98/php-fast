df13737ac14c6591148dcd113010902109a192c1
Ashwani <imakumar98@gmail.com>
2020-01-16 11:30:54 +0530

    adds endpoint to homepage

--- index.php
+++ index.php
@@ -1,13 +1,28 @@
 <?php
 
 
-    echo "Headers are given below \n\r";
+    if($_REQUEST['url']=='' && $_SERVER['REQUEST_METHOD']=='GET') {
+        echo "Welcome to PHP Fast";
+    }
 
 
+    
 
-    print_r($_REQUEST);
+    
 
-    echo $_SERVER['REQUEST_METHOD'];
 
+    if($_REQUEST['url']=='api/students' && $_SERVER['REQUEST_METHOD']=='GET') {
+
+        $result = array();
+
+        $result['data'] = array(
+            "name" => "Ashwani",
+            "age" => 20
+        );
+
+        echo json_encode($result);
+
+    }
+ 
 
 ?>
