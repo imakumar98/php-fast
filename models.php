6e7f262495905a4cfec277243e21334bd39de945
Ashwani <imakumar98@gmail.com>
2020-01-16 14:40:05 +0530

    define models

---
+++ models.php
@@ -0,0 +1,21 @@
+<?php
+
+
+$models = array();
+
+
+$models['student'] = array(
+    array('name', 'string', true),
+    array('age', 'number', true),
+);
+
+$models['book'] = array(
+    array('title', 'string', true),
+    array('price', 'number', true),
+);
+
+
+
+
+
+?>
